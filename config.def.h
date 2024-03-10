46a12383e75b4f020d65fa2b53f0b1be096b88f6
Raghuram Subramani <raghus2247@gmail.com>
2024-03-10 15:17:44 +0530

    change background color

--- config.def.h
+++ config.def.h
@@ -8,8 +8,8 @@ static const int sloppyfocus               = 1;  /* focus follows mouse */
 static const int bypass_surface_visibility = 0;  /* 1 means idle inhibitors will disable idle tracking even if it's surface isn't visible  */
 static const int smartborders              = 1;
 static const unsigned int borderpx         = 2;  /* border pixel of windows */
-static const float rootcolor[]             = COLOR(0x272727ff);
-static const float bordercolor[]           = COLOR(0x272727ff);
+static const float rootcolor[]             = COLOR(0x131417ff);
+static const float bordercolor[]           = COLOR(0x131417ff);
 static const float focuscolor[]            = COLOR(0xf2f0faff);
 static const float urgentcolor[]           = COLOR(0xf2f0faff);
 /* To conform the xdg-protocol, set the alpha to zero to restore the old behavior */
