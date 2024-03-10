a8ad68ceb757bd41bd0a1ac9ebcdddc23206ad6c
Raghuram Subramani <raghus2247@gmail.com>
2024-03-10 14:50:36 +0530

    update

--- config.def.h
+++ config.def.h
@@ -23,8 +23,8 @@ static int log_level = WLR_ERROR;
 
 /* Autostart */
 static const char *const autostart[] = {
-        "wbg", "/home/compromyse/Pictures/wallpaper.jpg", NULL,
-        "/home/compromyse/.bar.sh", NULL,
+        "wbg", "~/Pictures/wallpaper.jpg", NULL,
+        "~/.bar.sh", NULL,
         "way-displays", NULL,
         "dunst", NULL,
         "swayidle", "before-sleep", "swaylock", "lock", "swaylock", NULL,
@@ -134,7 +134,7 @@ static const char *upbrightness[] = { "brightnessctl", "set", "+5%", NULL };
 static const char *downbrightness[] = { "brightnessctl", "set", "5%-", NULL };
 
 static const char *termcmd[] = { "alacritty", NULL };
-static const char *menucmd[] = { "bemenu-run", "-i", "-p", "λ", "-b", "--hp", "8", "--fn", "'UbuntuMono Nerd Font Mono'", "--tb", "'#272727'", "--tf", "'#f2f0fa'", "--fb", "'#272727'", "--ff", "'f2f0fa'", "--cb", "'#272727'", "--cf", "'#f2f0fa'", "--nb", "'#272727'", "--nf", "'#f2f0fa'", "--hb", "'#f2f0fa'", "--hf", "'#272727'", "--fbb", "'#272727'", "--fbf", "'#f2f0fa'", "--sb", "'#f2f0fa'", "--sf", "'#272727'", "--ab", "'#272727'", "--af", "'#f2f0fa'", "--scb", "'#272727'", "--scf", "'#272727'", NULL };
+static const char *menucmd[] = { "~/.run.sh", NULL };
 static const char *lockcmd[] = { "swaylock", NULL };
 
 static const Key keys[] = {
