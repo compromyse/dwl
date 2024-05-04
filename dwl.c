fcc65f28ec1c295ac38d8944aca30afd25b56560
Raghuram Subramani <raghus2247@gmail.com>
2024-05-04 17:03:38 +0530

    update

--- dwl.c
+++ dwl.c
@@ -2502,8 +2502,8 @@ setup(void)
 	 * Xcursor themes to source cursor images from and makes sure that cursor
 	 * images are available at all scale factors on the screen (necessary for
 	 * HiDPI support). Scaled cursors will be loaded with each output. */
-	cursor_mgr = wlr_xcursor_manager_create(NULL, 24);
-	setenv("XCURSOR_SIZE", "24", 1);
+	cursor_mgr = wlr_xcursor_manager_create(NULL, 16);
+	setenv("XCURSOR_SIZE", "16", 1);
 
 	/*
 	 * wlr_cursor *only* displays an image on screen. It does not move around
